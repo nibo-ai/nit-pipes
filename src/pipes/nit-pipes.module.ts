e69f225c33b3c5966fdaeb8408e9b4345ff70d26
Adrian Ber <beradrian@yahoo.com>
2018-07-16 00:21:04 +0300

    Export sanitize pipe

--- src/pipes/nit-pipes.module.ts
+++ src/pipes/nit-pipes.module.ts
@@ -4,13 +4,15 @@ import { SafeUrlPipe } from './safe-url.pipe';
 import { InitialsPipe } from './initials.pipe';
 import { ArrayJoinPipe, ArrayImpureJoinPipe } from './array.join.pipe';
 import { IsArrayPipe } from './is.array.pipe';
+import { SanitizePipe } from './sanitize.pipe';
 
 const NIT_PIPES = [
 	SafeUrlPipe,
+	SanitizePipe,
 	InitialsPipe,
 	ArrayJoinPipe, ArrayImpureJoinPipe,
 	IsArrayPipe
-]
+];
 
 @NgModule({
 	declarations: NIT_PIPES,
@@ -19,5 +21,6 @@ const NIT_PIPES = [
 export class NitPipesModule { }
 
 export { SafeUrlPipe } from './safe-url.pipe';
+export { SanitizePipe } from "./sanitize.pipe";
 export { InitialsPipe } from './initials.pipe';
 export { ArrayJoinPipe, ArrayImpureJoinPipe } from './array.join.pipe';
