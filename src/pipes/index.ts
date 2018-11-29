9d05ea8be84f31c3389ea8a710db80d569f2f440
Adrian Ber <beradrian@yahoo.com>
2018-11-29 19:33:30 +0200

    Fix

--- src/pipes/index.ts
+++ src/pipes/index.ts
@@ -1,8 +1,10 @@
+export * from "./nit-pipes.module";
+
+export * from './array.join.pipe';
+export * from "./first.pipe";
+export * from './initials.pipe';
+export * from './is.array.pipe';
+export * from "./last.pipe";
+export * from "./load.pipe";
 export * from './safe-url.pipe';
 export * from "./sanitize.pipe";
-export * from './initials.pipe';
-export * from './array.join.pipe';
-export *  from "./load.pipe";
-export * from "./first.pipe";
-export * from "./last.pipe";
-export * from "./nit-pipes.module";
--- src/pipes/nit-pipes.module.ts
+++ src/pipes/nit-pipes.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 
-import { SafeUrlPipe } from './safe-url.pipe';
-import { InitialsPipe } from './initials.pipe';
 import { ArrayJoinPipe, ArrayImpureJoinPipe } from './array.join.pipe';
-import { IsArrayPipe } from './is.array.pipe';
-import { SanitizePipe } from './sanitize.pipe';
-import { LoadPipe } from './load.pipe';
 import { FirstPipe } from './first.pipe';
+import { InitialsPipe } from './initials.pipe';
+import { IsArrayPipe } from './is.array.pipe';
 import { LastPipe } from './last.pipe';
+import { LoadPipe } from './load.pipe';
+import { SafeUrlPipe } from './safe-url.pipe';
+import { SanitizePipe } from './sanitize.pipe';
 
 const NIT_PIPES = [
 	SafeUrlPipe,
