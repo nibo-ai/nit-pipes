ec2ca4dd65dfdea8174fcca9ebabd76da25c0d74
Adrian Ber <beradrian@yahoo.com>
2020-02-17 19:32:08 +0200

    Export all; Refactor

--- projects/nit-pipes/src/lib/index.ts
+++ projects/nit-pipes/src/lib/index.ts
@@ -1,14 +1,19 @@
 export * from "./nit-pipes.module";
 
-export * from './array.join.pipe';
+export * from "./array.join.pipe";
 export * from "./debounce.pipe";
 export * from "./first.pipe";
-export * from './initials.pipe';
-export * from './is.array.pipe';
+export * from "./initials.pipe";
+export * from "./is.array.pipe";
+export * from "./is.boolean.pipe";
+export * from "./is.date.pipe";
+export * from "./is.function.pipe";
+export * from "./is.null.pipe";
+export * from "./is.string.pipe";
 export * from "./last.pipe";
 export * from "./load.pipe";
-export * from './safe-url.pipe';
-export * from './safe.pipe';
+export * from "./safe-url.pipe";
+export * from "./safe.pipe";
 export * from "./sanitize.pipe";
-export * from './xfilter.pipe';
+export * from "./xfilter.pipe";
 
--- projects/nit-pipes/src/lib/nit-pipes.module.ts
+++ projects/nit-pipes/src/lib/nit-pipes.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 
 import { ArrayJoinPipe, ArrayImpureJoinPipe } from './array.join.pipe';
+import { DebouncePipe } from './debounce.pipe';
 import { FirstPipe } from './first.pipe';
 import { InitialsPipe } from './initials.pipe';
 import { IsArrayPipe } from './is.array.pipe';
-import { LastPipe } from './last.pipe';
-import { LoadPipe } from './load.pipe';
-import { SafeUrlPipe } from './safe-url.pipe';
-import { SanitizePipe } from './sanitize.pipe';
-import { SafePipe } from './safe.pipe';
-import { XFilterPipe } from './xfilter.pipe';
-import { DebouncePipe } from './debounce.pipe';
 import { IsBooleanPipe } from './is.boolean.pipe';
+import { IsDatePipe } from './is.date.pipe';
 import { IsFunctionPipe } from './is.function.pipe';
 import { IsNullPipe } from './is.null.pipe';
 import { IsStringPipe } from './is.string.pipe';
-import { IsDatePipe } from './is.date.pipe';
+import { LastPipe } from './last.pipe';
+import { LoadPipe } from './load.pipe';
+import { SafePipe } from './safe.pipe';
+import { SafeUrlPipe } from './safe-url.pipe';
+import { SanitizePipe } from './sanitize.pipe';
+import { XFilterPipe } from './xfilter.pipe';
 
 const NIT_PIPES = [
 	ArrayJoinPipe, ArrayImpureJoinPipe,
